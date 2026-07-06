30af6c9805394005a7847909265c4507548d0c5f
Mohmmed imran M <mohmmedimran064@gmail.com>
2026-07-06 13:02:21 +0530

    Update 02_create_tables.sql

--- 02_create_tables.sql
+++ 02_create_tables.sql
@@ -52,3 +52,19 @@ create table orders (
         references customers(customers_id)
 );
 
+
+create table Order_Items (
+    order_item_id int auto_increment primary key,
+    order_id int not null,
+    product_id int not null,
+    quantity int not null,
+    unit_price decimal(10,2) not null,
+
+    constraint fk_orderitems_order
+        foreign key (order_id)
+        references Orders(order_id),
+
+    constraint fk_orderitems_product
+        foreign key (product_id)
+        references Products(product_id)
+);
