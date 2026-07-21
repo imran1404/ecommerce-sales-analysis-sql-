c2790ee9203a933ae17b5fc876b606e119d44dcf
Mohmmed imran M <mohmmedimran064@gmail.com>
2026-07-21 10:19:57 +0530

    Update 102_Joins.sql

--- Queries/102_Joins.sql
+++ Queries/102_Joins.sql
@@ -1,1 +1,24 @@
+-- Joins Query.
+
+--1. List customers with their ordered product details
+
+select c.first_name, o.order_id, oi.product_id, p. product_name, p.category_id
+from customers c
+inner join orders o
+on c.customer_id = o.customer_id
+inner join order_items oi
+on oi.order_id = o.order_id
+inner join products p
+on p.product_id = oi.product_id;
+
+
+--2. Show Products with Their Category Names
+
+select p.product_id, p.product_name, c.category_name
+from products p
+inner join categories c
+on p.category_id = c.category_id
+order by product_id;
+
+
 
